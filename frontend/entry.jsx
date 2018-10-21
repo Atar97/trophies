6f3f94122b9e58a0bebcb50e46069ba42ccacc69
Austin <austin.cotant@gmail.com>
2018-10-20 17:20:34 -0700

    Add bootstrapped user script to root static page

--- frontend/entry.jsx
+++ frontend/entry.jsx
@@ -2,24 +2,23 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 
 import Root from './components/root';
-import createStore from './store';
+import configureStore from './store';
 
 document.addEventListener('DOMContentLoaded', ()=> {
   const rootEl = document.getElementById('root');
-  const store = createStore({});
-  // let store;
-  // if (window.currentUser.id) {
-  //   const preloadedState = {
-  //     entities: {
-  //       users: {[window.currentUser.id]: window.currentUser}
-  //     },
-  //     session: {id: window.currentUser.id}
-  //   };
-  //   store = configureStore(preloadedState);
-  //   delete window.currentUser;
-  // } else {
-  //   store = configureStore({});
-  // }
+  let store;
+  if (window.currentUser.id) {
+    const preloadedState = {
+      entities: {
+        users: {[window.currentUser.id]: window.currentUser}
+      },
+      session: {id: window.currentUser.id}
+    };
+    store = configureStore(preloadedState);
+    delete window.currentUser;
+  } else {
+    store = configureStore({});
+  }
 
   window.getState = store.getState;
   window.dispatch = store.dispatch;
