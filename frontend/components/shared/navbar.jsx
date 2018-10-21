46f708100a5fd7599cf50990819ea00b95484be7
Austin <austin.cotant@gmail.com>
2018-10-21 08:23:33 -0700

    Add bootstrap css to stylesheets

--- frontend/components/shared/navbar.jsx
+++ frontend/components/shared/navbar.jsx
@@ -5,8 +5,20 @@ class Navbar extends React.Component {
 
     render() {
         return (
-            null
-        );
+            <nav className="navbar navbar-expand-lg navbar-dark bg-primary">
+                <ul className="navbar-nav mr-auto">
+                    <li className="nav-item active">
+                        <Link className="nav-link" to="/">Home</Link>
+                    </li>
+                    <li className="nav-item">
+                        <Link className="nav-link" to="/awards">Awards</Link>
+                    </li>
+                    <li className="nav-item">
+                        <Link className="nav-link" to="/archive">Archive</Link>
+                    </li>
+                </ul>
+            </nav>        
+        ); 
     }
 }
 
